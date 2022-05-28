cbbf03c2bf1fe4e0de54183f4ca27626a1254935
zeynepsl <zeyneprsalman@gmail.com>
2022-05-28 14:41:24 +0300

    scheduler v2

---
+++ online-banking/src/main/java/patika/bootcamp/onlinebanking/service/otoPayment/OtoPaymentDebt.java
@@ -0,0 +1,13 @@
+package patika.bootcamp.onlinebanking.service.otoPayment;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OtoPaymentDebt {
+	
+	@Scheduled(cron = "0 10 10 15 * ?")
+	public void paymentDebt() {
+		//oto kredi borcu ödeme
+	}
+}
